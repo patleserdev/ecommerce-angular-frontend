e995e361690ddebba219245d65b8c4328fbec08e
patleserdev <leserpat@hotmail.com>
2025-08-21 13:42:53 +0200

    debug mobile view for login and register

--- src/app/shared/back-to-top/back-to-top.component.ts
+++ src/app/shared/back-to-top/back-to-top.component.ts
@@ -13,6 +13,7 @@ import { ResponsiveService } from '../../services/responsive.service.js';
 export class BackToTopComponent {
   showScrollTop = false;
   isMobile = false;
+  isHiding = false;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -21,8 +22,6 @@ export class BackToTopComponent {
     if (isPlatformBrowser(this.platformId)) {
       this.responsiveService.isMobile$.subscribe(isMobile => {
         this.isMobile = isMobile;
-
-        // Réinitialiser le bouton si on passe en mobile
         if (isMobile) {
           this.showScrollTop = false;
         }
@@ -33,22 +32,31 @@ export class BackToTopComponent {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     if (isPlatformBrowser(this.platformId)) {
-      // Ne rien faire en version mobile
-      if (this.isMobile) {
-        this.showScrollTop = false;
-        return;
-      }
+      const scrollPosition = window.scrollY;
+      const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrolledPercentage = (scrollPosition / totalHeight) * 100;
 
-      this.showScrollTop = window.scrollY > 100;
+      if (this.isMobile) {
+        this.showScrollTop = scrolledPercentage > 30;
+      } else {
+        this.showScrollTop = scrollPosition > 100;
+      }
     }
   }
 
   scrollToTop(): void {
     if (isPlatformBrowser(this.platformId)) {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+      if (this.isMobile) {
+        this.isHiding = true; // animation disparition mobile
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+
+        setTimeout(() => {
+          this.showScrollTop = false;
+          this.isHiding = false;
+        }, 1000); // durée animation
+      } else {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
     }
   }
 }
